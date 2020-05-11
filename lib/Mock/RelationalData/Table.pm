b53bf887bf1a048c8aa460480e7d69146037f372
Michael Conrad <mike@nrdvana.net>
2020-05-11 02:31:22 -0400

    Document the attributes of the tables

--- lib/Mock/RelationalData/Table.pm
+++ lib/Mock/RelationalData/Table.pm
@@ -2,14 +2,107 @@ package Mock::RelationalData::Table;
 use Moo 2;
 use Carp;
 
-has name         => is => 'rw', required => 1;
-has columns      => is => 'rw', required => 1, coerce => \&_coerce_columns;
-has column_order => is => 'rw', lazy => 1, builder => 1, predicate => 1;
-has primary_key  => is => 'rw', lazy => 1, builder => 1;
-has relations    => is => 'rw', default => sub { +{} };
+=head1 DESCRIPTION
 
-has rows         => is => 'rw', default => sub { [] };
-has row_by_key   => is => 'rw', default => sub { +{} };
+This class represents a table in the schema.
+It stores both the schema definition, and the rows that have been generated so far.
+
+=head1 ATTRIBUTES
+
+=head2 name
+
+Name of the table (or for DBIC, the name of the Result Source).  This module does not generate
+any SQL, so this is for your information only and does not need to match your database.
+
+=head2 columns
+
+A hashref of C<< { $column_name => \%column_info, ... } >>.  This may be passed to the
+constructor in full, or using several shorthand notations:
+
+=over
+
+=item ArrayRef
+
+This specifies the order of the columns if the C<column_order> attribute is not supplied.
+
+  [ $column_name => \%column_info, ... ]
+
+=item HashRef
+
+This does not specify an order of the columns.  The C<column_order> attribute will get its
+default value if not supplied.
+
+  { $column_name => \%column_info, ... }
+
+=item Scalar instead of column info
+
+If the C<%column_info> portion is a scalar or scalar ref, it is assumed to be the C<fill> field
+of the C<%column_info>.
+
+  [ $column_name => $fill, ... ]
+  # becomes
+  { $column_name => { name => $column_name, fill => $fill } }
+
+=back
+
+=head2 column_order
+
+An arrayref of the names of the columns in the order they should be presented.
+If not specified, this is built from the C<< column_info->{idx} >> values, or lacking that,
+alphabetical with primary key first.
+
+=head2 primary_key
+
+An arrayref of column names that compose the primary key.  This may be an empty arrayref for
+tables with no primary key.  This will be automatically built from any columns you supplied
+with a nonzero C<pk> attribute. (sorted by the pk attribute value)
+
+=head2 relations
+
+A hashref of relation names from this table to another table.  Each relation is of the form:
+
+  {
+    name => $name,
+  
+  # direct relations:
+  
+    cols        => \@self_column_names,
+    peer        => $peer_table_name,
+    peer_cols   => \@peer_column_names,
+    is_fk       => \$bool
+    cardinality => ...# '1:1', '1:N', 'N:1'
+  
+  # or many-many relations:
+  
+    rel         => $self_relation_to_peer,
+    peer_rel    => $peer_relation_to_destination,
+    cardinality => 'N:N'
+  }
+
+When constructing rows, you may specify data for a relation and (like in DBIC) it will create
+a related row.  If you don't specify data for the relation but it is a foreign key composed of
+not-null columns, it will also create the related row (with mock data).
+
+=head2 rows
+
+This is the current arrayref of all data rows for this table.  You should not set these
+directly; use the L</add_rows> method.  Rows are stored as arrayrefs in L</column_order>.
+
+=head2 row_by_key
+
+This is an index of the row data added so far.  It is a hashref of key names to row keys to
+row arrayrefs.
+
+=cut
+
+has name         => is => 'ro', required => 1;
+has columns      => is => 'ro', required => 1, coerce => \&_coerce_columns;
+has column_order => is => 'lazy';
+has primary_key  => is => 'lazy';
+has relations    => is => 'ro', default => sub { +{} };
+
+has rows         => is => 'rw', init_arg => undef, default => sub { [] };
+has row_by_key   => is => 'rw', init_arg => undef, default => sub { +{} };
 
 sub _pk_str {
 	my ($self, $record)= @_;
